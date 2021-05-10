402a7750a96785d8d4af4a7c5ef31cff2d39685d
Fergus Baker <fergusbkr@gmail.com>
2021-05-10 19:39:48 +0200

    Tag Exclusions (#4)

--- src/config.js
+++ src/config.js
@@ -8,4 +8,8 @@ const TOCREGEX = new RegExp(`${TOCSTART}[\\s\\S^<]*${TOCEND}`);
 const RE_TOCSTART = new RegExp(`^${TOCSTART}`);
 const RE_TOCEND = new RegExp(`^${TOCEND}`);
 
-module.exports = {TOCSTART, TOCEND, TOCREGEX, RE_TOCSTART, RE_TOCEND}
+// tags
+const TAG_REPLACE = /(::|[\s\.])/g;
+const TAG_EXCLUSIONS = /[\(\)`:;]/g;
+
+module.exports = {TOCSTART, TOCEND, TOCREGEX, RE_TOCSTART, RE_TOCEND, TAG_REPLACE, TAG_EXCLUSIONS}
